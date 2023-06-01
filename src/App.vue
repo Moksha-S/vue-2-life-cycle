bbf4c68c33eb26089d5bdb87475fc0c2ff189551
MokshaShah <moksha@simformsolutions.com>
2023-06-01 18:31:56 +0530

    vue 2 life cycle hooks basic demo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <LifeCycle />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LifeCycle from './components/LifeCycle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LifeCycle
   }
 }
 </script>
